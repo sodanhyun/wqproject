0585e375c161e6c3cdbdd5a999f0189f82880ada
sodanhyun <166380533+sodanhyun@users.noreply.github.com>
2024-10-30 14:27:14 +0900

    Create CustomHandshakeHandler.java

---
+++ src/main/java/com/codehows/wqproject/domain/ws/service/CustomHandshakeHandler.java
@@ -0,0 +1,18 @@
+package com.codehows.wqproject.domain.ws.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class CustomHandshakeHandler extends DefaultHandshakeHandler {
+    @Override
+    protected void handleInvalidUpgradeHeader(ServerHttpRequest request, ServerHttpResponse response) throws IOException {
+        // NOTE: Upgrade 헤더에 올바르지 않은 값이 전달되었을때 호출된다.
+        log.error("Method: {}, URI: {}, Principal: {}, Headers: {}", request.getMethod().name(), request.getURI(), request.getPrincipal(), request.getHeaders());
+        super.handleInvalidUpgradeHeader(request, response);
+    }
+}
